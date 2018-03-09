b20b063116b7bba95a1a400736cb43c4df562b70
Nilesh <mishranilesh012@gmail.com>
2018-03-09 11:09:28 +0530

    Home page added

--- GUI/src/app/navbar/navbar.component.ts
+++ GUI/src/app/navbar/navbar.component.ts
@@ -1,34 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
-  selector: 'app-navbar,ngbd-modal-basic',
+  selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  closeResult: string;
-  constructor(private modalService: NgbModal) { }
+  
+  constructor() { }
 
   ngOnInit() {
   }
 
-  open(content) {
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
+  
 }
