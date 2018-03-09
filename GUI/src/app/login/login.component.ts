bba42a183377915e4f41f09214feafe28c73db9b
Nilesh <mishranilesh012@gmail.com>
2018-03-10 01:25:17 +0530

    Updates in pages

--- GUI/src/app/login/login.component.ts
+++ GUI/src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login,ngbd-buttons-radio',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
+  model = 1;
   constructor() { }
 
   ngOnInit() {
